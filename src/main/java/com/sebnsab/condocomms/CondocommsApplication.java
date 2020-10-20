a7d9522ff9cc1b263e0377cb5f6f972f1f4003d1
Rommel Santiago <rommel@Jeepoy-Macbook-Pro.local>
2020-10-20 11:56:18 +1100

    Initial check in

---
+++ src/main/java/com/sebnsab/condocomms/CondocommsApplication.java
@@ -0,0 +1,13 @@
+package com.sebnsab.condocomms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CondocommsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CondocommsApplication.class, args);
+	}
+
+}
